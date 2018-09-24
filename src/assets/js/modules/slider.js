63fc9d8efee62b826272829540f51a8eab6e627c
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-09-24 20:16:56 +0300

    done slider

--- src/assets/js/modules/slider.js
+++ src/assets/js/modules/slider.js
@@ -1,1 +1,15 @@
-console.log('slider.js is work');
+import Glide from '../../../../node_modules/@glidejs/glide';
+
+new Glide('.slider', {
+  type: 'slider',
+  startAt: 1,
+  perview: 1,
+  autoplay: 3000,
+  hoverpause: true,
+  keyboard: true,
+  swipeThreshold: 80,
+  dragThreshold: 120,
+  animationDuration: 1000,
+  rewind: true,
+  rewindDuration: 3000
+}).mount();
