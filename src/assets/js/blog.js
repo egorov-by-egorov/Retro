2d2323ef041a0557aff3ef9229a5271c0fe0aadb
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-10-13 16:25:33 +0300

    upgrade section Blog

--- src/assets/js/blog.js
+++ src/assets/js/blog.js
@@ -1,102 +1,103 @@
-const user1 = 'https://jsonplaceholder.typicode.com/users/1/posts';
-const user2 = 'https://jsonplaceholder.typicode.com/users/2/posts';
-const user3 = 'https://jsonplaceholder.typicode.com/users/3/posts';
-// const photoURL = 'https://jsonplaceholder.typicode.com/photos';
-function get(user) {
-  fetch(user)
-    .then(response => response.json())
-    .then(posts => {
-      showPosts(posts);
-    })
-    .catch(err => console.log(err));
+export function getBlogPosts() {
+  const photos = getPhotos();
+  const posts = getPosts();
+  Promise.all([photos, posts]).then(arr => console.log(arr));
 }
 
-// function getPhoto(photos) {
-//   fetch(photos)
-//     .then(response => response.json())
-//     .then(photos => showPosts(photos))
-//     .catch(err => console.log(err));
+// function splitArraies(posts, photos) {
+//   const generalPosts = [];
+//   for (const post of posts) {
+//   }
 // }
 
-function showPosts(posts) {
-  posts.forEach((post, index) => {
-    if (index >= 0 && index < 1) {
-      createArticle(post);
-    }
-  });
-}
-const list = document.querySelector('.article__list');
-function createArticle(obj) {
-  const li = document.createElement('li');
-  li.classList.add('article__item');
+const getPhotos = () => {
+  fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(response => response.json())
+    .then(arr => arr.splice(0, 3))
+    .catch(err => console.log(err));
+};
 
-  const article = document.createElement('article');
-  article.classList.add('article__article');
+const getPosts = () => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(arr => arr.splice(0, 3))
+    .catch(err => console.log(err));
+};
 
-  const articleWindow = document.createElement('div');
-  articleWindow.classList.add('article__window');
+// function showPosts(posts) {
+//   const articleBox = document.querySelector('.article');
+//   const list = document.querySelector('.article__list');
 
-  const img = document.createElement('img');
-  img.classList.add('article__img');
-  img.setAttribute('src', '');
-  img.setAttribute('alt', 'Article Photo');
+//   for (const post of posts) {
+//     const article = createPost(post);
+//     list.appendChild(article);
+//   }
 
-  const articleDescBox = document.createElement('div');
-  articleDescBox.classList.add('article__desc-box');
+//   articleBox.appendChild(list);
+// }
 
-  const title = document.createElement('div');
-  title.classList.add('article__title');
-  title.textContent = obj.title;
+// function createPost(post) {
+//   const li = document.createElement('li');
+//   li.classList.add('article__item');
 
-  const breadcrumbs = document.createElement('div');
-  breadcrumbs.classList.add('article__breadcrumbs');
+//   const article = document.createElement('article');
+//   article.classList.add('article__article');
 
-  const date = document.createElement('time');
-  date.classList.add('article__data');
-  date.setAttribute('date', '07.28.2014');
-  date.textContent = obj.id;
+//   const articleWindow = document.createElement('div');
+//   articleWindow.classList.add('article__window');
 
-  const user = document.createElement('div');
-  user.classList.add('article__user');
-  user.textContent = obj.id;
+//   const img = document.createElement('img');
+//   img.classList.add('article__img');
+//   img.setAttribute('src', '');
+//   img.setAttribute('alt', 'Article Photo');
 
-  const theme = document.createElement('div');
-  theme.classList.add('article__theme');
+//   const articleDescBox = document.createElement('div');
+//   articleDescBox.classList.add('article__desc-box');
 
-  const answer = document.createElement('button');
-  answer.classList.add('article__answer');
-  answer.setAttribute('type', 'button');
-  answer.textContent = obj.id + ' ' + 'response';
+//   const title = document.createElement('div');
+//   title.classList.add('article__title');
+//   title.textContent = post.title;
 
-  const line = document.createElement('div');
-  line.classList.add('article__line');
+//   const breadcrumbs = document.createElement('div');
+//   breadcrumbs.classList.add('article__breadcrumbs');
 
-  const desc = document.createElement('div');
-  desc.classList.add('article__desc');
-  desc.textContent = obj.body;
+//   const date = document.createElement('time');
+//   date.classList.add('article__data');
+//   date.setAttribute('date', '07.28.2014');
+//   date.textContent = post.id;
 
-  list.appendChild(li);
-  li.appendChild(article);
-  articleWindow.appendChild(img);
-  articleDescBox.appendChild(title);
-  breadcrumbs.appendChild(date);
-  breadcrumbs.appendChild(user);
-  breadcrumbs.appendChild(theme);
-  breadcrumbs.appendChild(answer);
-  articleDescBox.appendChild(breadcrumbs);
-  articleDescBox.appendChild(line);
-  articleDescBox.appendChild(desc);
+//   const user = document.createElement('div');
+//   user.classList.add('article__user');
+//   user.textContent = post.id;
 
-  article.appendChild(articleWindow);
-  article.appendChild(articleDescBox);
-}
+//   const theme = document.createElement('div');
+//   theme.classList.add('article__theme');
 
-const addBtn = document.querySelector('.article__btn-add');
+//   const answer = document.createElement('button');
+//   answer.classList.add('article__answer');
+//   answer.setAttribute('type', 'button');
+//   answer.textContent = post.id + ' ' + 'response';
 
-addBtn.addEventListener('click', e => {
-  get(user1);
-  get(user2);
-  get(user3);
-  // getPhoto(photoURL);
-  addBtn.style.display = 'none';
-});
+//   const line = document.createElement('div');
+//   line.classList.add('article__line');
+
+//   const desc = document.createElement('div');
+//   desc.classList.add('article__desc');
+//   desc.textContent = post.body;
+
+//   articleWindow.appendChild(img);
+//   articleDescBox.appendChild(title);
+//   breadcrumbs.appendChild(date);
+//   breadcrumbs.appendChild(user);
+//   breadcrumbs.appendChild(theme);
+//   breadcrumbs.appendChild(answer);
+//   articleDescBox.appendChild(breadcrumbs);
+//   articleDescBox.appendChild(line);
+//   articleDescBox.appendChild(desc);
+
+//   article.appendChild(articleWindow);
+//   article.appendChild(articleDescBox);
+//   li.appendChild(article);
+
+//   return li;
+// }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import './server.php';
 import './assets/js/hero';
 import './assets/js/about';
 import './assets/js/portfolio';
-import './assets/js/blog';
 import './assets/js/form';
+import { getBlogPosts } from './assets/js/blog';
 
 // For All SVG
 function requireAll(r) {
   r.keys().forEach(r);
 }
 requireAll(require.context('./assets/images/svg/', true, /\.svg$/));
+window.addEventListener('load', () => {
+  getBlogPosts();
+});
