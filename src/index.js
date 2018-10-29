ce60e3aed840e6820ae0dd059dadf1099a1d1476
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-10-29 18:43:54 +0300

    return to for shows in gh-pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import './assets/js/modules/slider';
+import Glide from '@glidejs/glide';
 import './index.scss';
 import './assets/js/form';
-import { getSkills } from './assets/js/about';
 import './assets/js/portfolio';
+import { getSkills } from './assets/js/about';
 import { getBlogPosts } from './assets/js/blog';
+
+// Slider
+new Glide('.slider', {
+  type: 'slider',
+  startAt: 1,
+  perview: 1,
+  autoplay: 3000,
+  hoverpause: true,
+  keyboard: true,
+  swipeThreshold: 80,
+  dragThreshold: 120,
+  animationDuration: 1000,
+  rewind: true,
+  rewindDuration: 3000
+}).mount();
 
 // For All SVG
 function requireAll(r) {
@@ -13,6 +28,5 @@ requireAll(require.context('./assets/images/svg/', true, /\.svg$/));
 
 window.addEventListener('load', () => {
   getSkills();
-  // getFilter();
   getBlogPosts();
 });
